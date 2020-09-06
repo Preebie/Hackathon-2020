a52f9de35d6c1982f0c49b5ce25b19c16ce1aa32
Abhishek <abhimore74@gmail.com>
2020-09-05 23:01:34 -0400

    Firebase Helper

--- FirebaseHelper.py
+++ FirebaseHelper.py
@@ -1,2 +1,41 @@
 #Use this to configure functions to make calls to Google Firebase
-print("Big Booty tits")
+
+from Student import Student
+import firebase_admin
+from firebase_admin import db
+from firebase_admin import credentials
+
+cred = credentials.Certificate("howdy-hack-firebase-adminsdk-yeun6-f6e2df0815.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://howdy-hack.firebaseio.com"
+})
+
+def getStudents(checkpoint):
+#Returns a list of Student objects at the specified checkpoint
+    ref = db.reference(checkpoint)
+    result = ref.get()
+    if result:
+        students = []
+        for key, value in result.items():
+            students.append(Student(key, value))
+        return students
+    else:
+        return []
+
+def pushStudent(checkpoint, name, email):
+#Adds a student to the checkpoint
+    ref = db.reference(checkpoint)
+    ref.update({
+        name: email
+    })
+
+def removeStudent(checkpoint, name):
+#Removes student from checkpoint
+    ref = db.reference(checkpoint)
+    result = ref.get()
+    if result:
+        newDict = {}
+        for key, value in result.items():
+            if not key == name:
+                newDict.update({key: value})
+        ref.set(newDict)
