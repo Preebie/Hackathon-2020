771bfbaeec6d746f8dffb49aef335375f0df6550
rishiamarnath1 <rishiamarnath1@gmail.com>
2020-09-05 20:49:38 -0500

    Started app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 #Use this for any flask implementation
+from flask import Flask
+
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return "Hello!"
+
+if __name__ == "__main__":
+    app.run()
